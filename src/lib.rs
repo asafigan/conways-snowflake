4e291ab6f670fe5e9693c66cafb333b0d4face1a
Andrew Safigan <asafigan@gmail.com>
2020-02-25 19:31:22 -0500

    double buffer optimization

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,8 @@ pub enum Cell {
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: Vec<Cell>,
+    last_generation: Vec<Cell>,
+    next_generation: Vec<Cell>,
 }
 
 #[wasm_bindgen]
@@ -37,7 +38,7 @@ impl Universe {
     pub fn new(width: u32, height: u32) -> Universe {
         utils::set_panic_hook();
 
-        let cells = (0..width * height)
+        let cells: Vec<_> = (0..width * height)
             .map(|i| {
                 if i % 2 == 0 || i % 7 == 0 {
                     Cell::Alive
@@ -50,7 +51,8 @@ impl Universe {
         Universe {
             width,
             height,
-            cells,
+            last_generation: cells.clone(),
+            next_generation: cells,
         }
     }
 
@@ -58,7 +60,7 @@ impl Universe {
         static BLACK: u32 = 0xFF_00_00_00;
         static WHITE: u32 = 0xFF_FF_FF_FF;
         let (_, pixels, _) = unsafe { buffer.align_to_mut::<u32>() };
-        for (cell, pixel) in self.cells.iter().zip(pixels.iter_mut()) {
+        for (cell, pixel) in self.next_generation.iter().zip(pixels.iter_mut()) {
             match cell {
                 Cell::Alive => *pixel = BLACK,
                 Cell::Dead => *pixel = WHITE,
@@ -90,39 +92,39 @@ impl Universe {
         };
 
         let nw = self.get_index(north, west);
-        count += self.cells[nw] as u8;
+        count += self.last_generation[nw] as u8;
 
         let n = self.get_index(north, column);
-        count += self.cells[n] as u8;
+        count += self.last_generation[n] as u8;
 
         let ne = self.get_index(north, east);
-        count += self.cells[ne] as u8;
+        count += self.last_generation[ne] as u8;
 
         let w = self.get_index(row, west);
-        count += self.cells[w] as u8;
+        count += self.last_generation[w] as u8;
 
         let e = self.get_index(row, east);
-        count += self.cells[e] as u8;
+        count += self.last_generation[e] as u8;
 
         let sw = self.get_index(south, west);
-        count += self.cells[sw] as u8;
+        count += self.last_generation[sw] as u8;
 
         let s = self.get_index(south, column);
-        count += self.cells[s] as u8;
+        count += self.last_generation[s] as u8;
 
         let se = self.get_index(south, east);
-        count += self.cells[se] as u8;
+        count += self.last_generation[se] as u8;
 
         count
     }
 
     pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
+        std::mem::swap(&mut self.last_generation, &mut self.next_generation);
 
         for row in 0..self.height {
             for col in 0..self.width {
                 let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
+                let cell = self.last_generation[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
                 let next_cell = match (cell, live_neighbors) {
@@ -131,11 +133,9 @@ impl Universe {
                     _ => Cell::Dead,
                 };
 
-                next[idx] = next_cell;
+                self.next_generation[idx] = next_cell;
             }
         }
-
-        self.cells = next;
     }
 
     pub fn width(&self) -> u32 {
@@ -147,7 +147,7 @@ impl Universe {
     }
 
     pub fn clear(&mut self) {
-        self.cells = (0..self.width * self.height).map(|_i| Cell::Dead).collect();
+        self.next_generation = (0..self.width * self.height).map(|_i| Cell::Dead).collect();
     }
 }
 
@@ -162,7 +162,7 @@ impl Universe {
     pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
         for (row, col) in cells.iter().cloned() {
             let idx = self.get_index(row, col);
-            self.cells[idx] = Cell::Alive;
+            self.next_generation[idx] = Cell::Alive;
         }
     }
 }
